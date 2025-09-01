6be69e1de501df6d1eb16e624c782cf978dba8b8
Joaquin Coronado <joaquinycoronado@gmail.com>
2025-08-31 22:46:07 -0700

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is my first java app. It prints out Hello
+ * World and my name to the console.
+ * @author Joaquin Coronado, joaquinycoronado@gmail.com
+ * @since 8/31/2025
+ * @version v1.0
+ */
+public class HelloWorld
+{
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Joaquin Coronado");
+    }
+    }
