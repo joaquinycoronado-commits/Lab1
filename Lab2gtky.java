3fc4f1cedb29803ae90571d38f462e2ae8b5f79a
Joaquin Coronado <joaquinycoronado@gmail.com>
2025-09-07 23:44:12 -0700

    Lab 2 Get to Know You assignment

---
+++ Lab2gtky.java
@@ -0,0 +1,14 @@
+
+/**
+ * This is a three-line program with my name, City College major
+ * and an interesting fact about me.
+ *
+ * @author Joaquin Coronado, joaquinycoronado@gmail.com
+ * @version v1.0
+ */
+public class Lab2gtky
+{
+    public static void main (String[] args) {
+    System.out.println("My name is Joaquin Coronado.\nI am a Commercial Music major at San Diego City College.\n...and I have probably tried foods from around 120 different countries so far (and am hoping to tackle the rest this year).");
+}
+}
---
+++ Lab2imprecision.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Lab2imprecision here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lab2imprecision
+{
+    public static void main(String[] args) {
+    double x = 12345.6789e200;
+    double y = 1/x;
+    double z = x*y;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    System.out.println(1-z);
+    }
+}
